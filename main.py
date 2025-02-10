7e8c03deb22a745063261059f214feaeee2a6e04
nitro <user@users-MacBook-Pro.local>
2025-02-09 17:34:10 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+print("this is test class")
+
+
+dictt = {
+"male": "female",
+
+"key": "lock",
+
+"oraneg": "mango"
+
+
+
+
+
+
+}
+
+
+
+
+print(dictt['key'])
+
+for values in dictt.items():
+    print(values)
+
+
+
+
+
+st = {'test', 3, "five", 77}
+
+print("this has been done")
+
+
+lstset = list(st)
+
+print(lstset[2])
